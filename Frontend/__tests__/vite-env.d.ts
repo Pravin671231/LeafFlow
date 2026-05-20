22a2edd44e49c1282475a5ae8ca71493b766b676
Pravin671231 <pravinkumar671231@gmail.com>
2026-05-21 02:07:13 +0530

    refactor: update package.json to rename workspaces and add new ESLint plugins

--- Frontend/__tests__/vite-env.d.ts
+++ Frontend/__tests__/vite-env.d.ts
@@ -3,7 +3,7 @@ import type { TestingLibraryMatchers } from '@testing-library/jest-dom/matchers'
 
 declare module 'vitest' {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-  interface Assertion<T = any> extends TestingLibraryMatchers<T, void> {}
+  interface Assertion<T = unknown> extends TestingLibraryMatchers<T, void> {}
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface AsymmetricMatchersContaining extends TestingLibraryMatchers<unknown, void> {}
 }
